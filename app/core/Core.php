b51df8e52e947ec13004598c1a3be5919c5ef13c
jmucak <jmucak22@gmail.com>
2024-09-02 13:14:58 +0200

    add cpt and taxonomies provider

--- app/core/Core.php
+++ app/core/Core.php
@@ -2,16 +2,25 @@
 
 namespace wpThemeTemplate\core;
 
-class Core {
-	public function init() : void {
+use wpThemeTemplate\providers\CPTProvider;
+use wpThemeTemplate\providers\TaxonomiesProvider;
 
+class Core {
+	public function init(): void {
 		add_action( 'wp_enqueue_scripts', array( $this, 'add_scripts' ) );
+
+		$this->register_providers();
 	}
 
-	public function add_scripts() : void {
+	public function add_scripts(): void {
 		$url = INCLUDE_URL . 'static/dist/style.css';
 
-		wp_enqueue_style( 'theme-css', trailingslashit(INCLUDE_URL) . 'static/dist/style.css', null, '1.0.0', false );
-		wp_enqueue_script( 'theme-js', trailingslashit(INCLUDE_URL) . 'static/dist/bundle.js', null, '1.0.0', true );
+		wp_enqueue_style( 'theme-css', trailingslashit( INCLUDE_URL ) . 'static/dist/style.css', null, '1.0.0', false );
+		wp_enqueue_script( 'theme-js', trailingslashit( INCLUDE_URL ) . 'static/dist/bundle.js', null, '1.0.0', true );
+	}
+
+	private function register_providers(): void {
+		( new CPTProvider() )->init();
+		( new TaxonomiesProvider() )->init();
 	}
 }
---
+++ app/providers/CPTProvider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace wpThemeTemplate\providers;
+
+class CPTProvider {
+	// Add cpt name, always should be singluar name
+	const MOVIES_CPT = 'movie';
+	const DOMAIN = 'wp-theme-template';
+
+	public function init(): void {
+		foreach ( $this->get_config() as $post_type => $args ) {
+			register_post_type( $post_type, $args );
+		}
+	}
+
+	public function get_config(): array {
+		return array(
+			self::MOVIES_CPT => $this->get_movie_args(),
+		);
+	}
+
+	private function get_movie_args(): array {
+		$label        = 'Movie';
+		$label_plural = 'Movies';
+
+		return array(
+			'labels'              => array(
+				'name'                  => sprintf( __( '%s', self::DOMAIN ), $label_plural ),
+				'singular_name'         => sprintf( __( '%s', self::DOMAIN ), $label ),
+				'all_items'             => sprintf( __( 'All %s', self::DOMAIN ), $label_plural ),
+				'menu_name'             => sprintf( __( '%s', 'Admin menu name', self::DOMAIN ), $label_plural ),
+				'add_new'               => __( 'Add New', self::DOMAIN ),
+				'add_new_item'          => sprintf( __( 'Add new %s', self::DOMAIN ), $label ),
+				'edit'                  => __( 'Edit', self::DOMAIN ),
+				'edit_item'             => sprintf( __( 'Edit %s', self::DOMAIN ), $label ),
+				'new_item'              => sprintf( __( 'New %s', self::DOMAIN ), $label ),
+				'view_item'             => sprintf( __( 'View %s', self::DOMAIN ), $label ),
+				'view_items'            => sprintf( __( 'View %s', self::DOMAIN ), $label_plural ),
+				'search_items'          => sprintf( __( 'Search %s', self::DOMAIN ), $label_plural ),
+				'not_found'             => sprintf( __( 'No %s found', self::DOMAIN ), $label_plural ),
+				'not_found_in_trash'    => sprintf( __( 'No %s found in trash', self::DOMAIN ), $label_plural ),
+				'parent'                => sprintf( __( 'Parent %s', self::DOMAIN ), $label ),
+				'featured_image'        => sprintf( __( '%s image', self::DOMAIN ), $label ),
+				'set_featured_image'    => sprintf( __( 'Set %s image', self::DOMAIN ), $label ),
+				'remove_featured_image' => sprintf( __( 'Remove %s image', self::DOMAIN ), $label ),
+				'use_featured_image'    => sprintf( __( 'Use as %s image', self::DOMAIN ), $label ),
+				'insert_into_item'      => sprintf( __( 'Insert into %s', self::DOMAIN ), $label ),
+				'uploaded_to_this_item' => sprintf( __( 'Uploaded to this %s', self::DOMAIN ), $label ),
+				'filter_items_list'     => sprintf( __( 'Filter %s', self::DOMAIN ), $label_plural ),
+				'items_list_navigation' => sprintf( __( '%s navigation', self::DOMAIN ), $label_plural ),
+				'items_list'            => sprintf( __( '%s list', self::DOMAIN ), $label_plural ),
+			),
+			'description'         => '',
+			'supports'            => array( 'title', 'editor', 'thumbnail' ),
+			'public'              => true,
+			'show_ui'             => true,
+			'publicly_queryable'  => true,
+			'exclude_from_search' => false,
+			'hierarchical'        => false, // Hierarchical causes memory issues - WP loads all records!
+			'rewrite'             => array(
+				'with_front' => false,
+				'slug'       => self::MOVIES_CPT,
+			),
+			'query_var'           => true,
+			'has_archive'         => false,
+			'show_in_nav_menus'   => true,
+			'show_in_rest'        => true,
+			'menu_icon'           => null,
+			'acfe_admin_archive'  => false,
+		);
+	}
+}
