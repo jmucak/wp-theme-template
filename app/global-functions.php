4b2166e57b25b627e0a36da8d0666840147fd28a
jmucak <jmucak22@gmail.com>
2024-09-20 22:34:00 +0200

    remove template loader service update global functions

--- app/global-functions.php
+++ app/global-functions.php
@@ -1,7 +1,6 @@
 <?php
 
 use jmucak\wpHelpersPack\helpers\TemplateLoaderHelper;
-use wsytesTheme\services\TemplateLoaderService;
 
 
 /**
@@ -11,9 +10,7 @@ use wsytesTheme\services\TemplateLoaderService;
 function get_partial( string $path, array $data = array(), bool $html = false ): bool|string|null {
 	$file_path = TEMPLATE_PATH . 'partials/' . $path . '.php';
 
-//	return TemplateLoaderService::get_instance()->get_partial( $file_path, $data, $html );
-
-	return TemplateLoaderHelper::get_instance()->get_partial($file_path, $data, $html);
+	return TemplateLoaderHelper::get_instance()->get_partial( $file_path, $data, $html );
 }
 
 /**
@@ -22,21 +19,21 @@ function get_partial( string $path, array $data = array(), bool $html = false ):
 function get_icon( string $path, bool $html = false ): bool|string|null {
 	$file_path = TEMPLATE_PATH . 'static/icons/' . $path . '.php';
 
-	return TemplateLoaderService::get_instance()->get_partial( $file_path, array(), $html );
+	return TemplateLoaderHelper::get_instance()->get_partial( $file_path, array(), $html );
 }
 
 /**
  * Function to get base url to static folder
  */
 function get_static_bu( string $url ): string {
-	return TemplateLoaderService::get_instance()->get_base_url( $url, 'static/' );
+	return TEMPLATE_URI . 'static/' . trim( $url );
 }
 
 /**
  * Function to get absolute url to static folder
  */
 function get_static_au( string $url ): string {
-	return TemplateLoaderService::get_instance()->get_absolute_url( $url, 'static/' );
+	return TEMPLATE_PATH . 'static/' . trim( $url );
 }
 
 /**
