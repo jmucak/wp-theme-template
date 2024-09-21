f0f7bc21ac6101d0175eb31fa22d9502e5132228
jmucak <jmucak22@gmail.com>
2024-09-21 23:15:18 +0200

    update theme provider

--- app/providers/ThemeServiceProvider.php
+++ app/providers/ThemeServiceProvider.php
@@ -2,30 +2,28 @@
 
 namespace wsytesTheme\providers;
 
+use jmucak\wpHelpersPack\hooks\BlockSettingsEvent;
 use jmucak\wpHelpersPack\providers\AssetProvider;
-use jmucak\wpHelpersPack\providers\BlockProvider;
-use jmucak\wpHelpersPack\providers\CPTProvider;
 use jmucak\wpHelpersPack\providers\ServiceProvider;
-use jmucak\wpHelpersPack\subscribers\BlockSubscriber;
 use jmucak\wpImagePack\providers\ImageProvider;
 use wsytesTheme\services\BlockService;
 
 class ThemeServiceProvider {
 	public function init(): void {
-		$this->register_providers();
-	}
-
-	private function register_providers(): void {
 		// register theme scripts
 		add_action( 'wp_enqueue_scripts', array( new AssetProvider( ConfigProvider::get_assets_config() ), 'register' ) );
 
+		add_action( 'init', array( $this, 'register_providers' ) );
+	}
+
+	public function register_providers(): void {
 		$service_provider = new ServiceProvider();
 		$service_provider->register_post_types( ConfigProvider::get_post_types_config() );
 		$service_provider->register_taxonomies( ConfigProvider::get_taxonomies_config() );
 
 		$block_config = ConfigProvider::get_blocks_config( new BlockService() );
-		$service_provider->register_blocks($block_config['blocks']);
-		(new BlockSubscriber($block_config));
+		$service_provider->register_blocks( $block_config['blocks'] );
+		( new BlockSettingsEvent( $block_config ) );
 
 		// Image pack config
 		( new ImageProvider( array(
