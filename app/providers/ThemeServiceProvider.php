847a83676d720a619565530d88b59475843ef739
jmucak <jmucak22@gmail.com>
2024-10-02 21:36:48 +0200

    update theme service provider

--- app/providers/ThemeServiceProvider.php
+++ app/providers/ThemeServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace wsytesTheme\providers;
 
-use jmucak\wpCoreManagerPack\providers\OptimizationProvider;
 use jmucak\wpImagePack\providers\ImageProvider;
-use jmucak\wpServiceProviderPack\ServiceProvider;
+use jmucak\wpServiceManagerPack\ServiceManager;
 use wsytesTheme\helpers\MenuHelper;
 use wsytesTheme\hooks\CPTControllerHook;
 
@@ -13,11 +12,16 @@ class ThemeServiceProvider {
 		// register theme scripts
 		$this->register_providers();
 		$this->register_hooks();
-		$this->add_theme_supports();
+
+		// Hide wordpress version from the head
+		remove_action( 'wp_head', 'wp_generator' );
 	}
 
 	public function register_providers(): void {
-		$service_provider = new ServiceProvider(array(
+		$service_manager = new ServiceManager();
+
+		// Register WP services
+		$service_manager->register_services(array(
 			'assets'               => AssetsProvider::get_config(),
 			'post_types'           => CPTProvider::get_config()['post_types'],
 			'taxonomies'           => CPTProvider::get_config()['taxonomies'],
@@ -27,11 +31,19 @@ class ThemeServiceProvider {
 				MenuHelper::HEADER_MENU_LOCATION => 'Header Menu',
 				MenuHelper::FOOTER_MENU_LOCATION => 'Footer Menu',
 			),
-			'query_vars'           => array( 'relation' ),
-			'register_cpt_filter'  => true,
-			'rest_route_namespace' => RESTProvider::get_api_namespace(),
-		) );
-		$service_provider->register_services();
+			'theme_supports' => array('title-tag', 'align-wide', 'post-thumbnails', 'editor-styles'),
+		));
+
+		// Deactivate WP Core services
+		$service_manager->deactivate_services(array(
+			'comments'    => true,
+			'wp_embeds'   => true,
+			'wp_emoji'    => true,
+			'posts'       => true,
+			'scripts'     => true,
+			'short_links' => true,
+		));
+
 
 		// Image pack config
 		$image_provider = new ImageProvider( array(
@@ -44,33 +56,10 @@ class ThemeServiceProvider {
 			'deregister_image_sizes' => array( '1536x1536', '2048x2048' ),
 		) );
 		$image_provider->register();
-
-		OptimizationProvider::register( array(
-			'deactivate_comments'    => true,
-			'deactivate_wp_embeds'   => true,
-			'deactivate_wp_emoji'    => true,
-			'deactivate_posts'       => true,
-			'deactivate_scripts'     => true,
-			'deactivate_short_links' => true,
-		) );
 	}
 
 	private function register_hooks(): void {
 		// Register hooks
 		( new CPTControllerHook() )->init();
-	}
-
-	/**
-	 * @url https://developer.wordpress.org/reference/functions/add_theme_support/
-	 * @return void
-	 */
-	private function add_theme_supports(): void {
-		add_theme_support( 'title-tag' );
-		add_theme_support( 'align-wide' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'editor-styles' );
-
-		// Hide wordpress version from the head
-		remove_action( 'wp_head', 'wp_generator' );
 	}
 }
