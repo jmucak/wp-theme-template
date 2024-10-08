c399eebd6cca781aca383093ee195389cd404663
tomo <tjezidzic@outlook.com>
2024-10-08 15:19:41 +0200

    frontend tweaks - page markup

--- templates/home-template.php
+++ templates/home-template.php
@@ -3,19 +3,18 @@
 
 get_header();
 ?>
+<main>
+    <div class="o-page">
+		<?php
+		get_partial( 'layout/navigation' );
+		?>
+        <div class="o-page__inner">
+            <h1>Home template</h1>
+        </div>
+		<?php
+		get_partial( 'layout/footer' );
+		get_footer();
+		?>
+    </div>
+</main>
 
-<div>
-
-    <?php
-    get_partial('layout/navigation');
-    ?>
-    <h1>Home template</h1>
-
-
-    <?php
-    get_partial('layout/footer');
-    ?>
-</div>
-
-<?php
-get_footer();
