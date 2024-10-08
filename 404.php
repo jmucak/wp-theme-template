fad0f142b5392afc01e9abdf15eb6fd367a1d110
tomo <tjezidzic@outlook.com>
2024-10-08 15:23:42 +0200

    frontend tweaks - page markup

--- 404.php
+++ 404.php
@@ -10,8 +10,20 @@
 get_header();
 
 ?>
-    <h1>
-        Page not found
-    </h1>
-<?php
-get_footer(); ?>
+<main>
+    <div class="o-page">
+		<?php
+		get_partial( 'layout/navigation' );
+		?>
+        <div class="o-page__inner">
+            <h1>
+                Page not found
+            </h1>
+        </div>
+		<?php
+		get_partial( 'layout/footer' );
+		get_footer();
+		?>
+    </div>
+</main>
+
