8b6f5c381f5178c78a644247664c473c5d8463a4
jmucak <jmucak22@gmail.com>
2024-10-10 16:08:12 +0200

    update cpt factory to look for - and _

--- app/factories/CPTFactory.php
+++ app/factories/CPTFactory.php
@@ -6,7 +6,20 @@ use wsytesTheme\interfaces\CPTFilterServiceInterface;
 
 class CPTFactory {
 	public static function get_service( string $post_type ): ?CPTFilterServiceInterface {
-		$service_class = 'wsytesTheme\\services\\'. ucfirst( $post_type ) . 'Service';
+		if ( str_contains( $post_type, '-' ) || str_contains( $post_type, '_' ) ) {
+			// Replace hyphens and underscores with spaces
+			$string = str_replace( [ '-', '_' ], ' ', $post_type );
+
+			// Capitalize the first letter of each word
+			$string = ucwords( $string );
+
+			// Remove spaces
+			$string = str_replace( ' ', '', $string );
+		} else {
+			$string = ucfirst( $post_type );
+		}
+
+		$service_class = 'wsytesTheme\\services\\' . $string . 'Service';
 
 		if ( class_exists( $service_class ) ) {
 			return new $service_class;
