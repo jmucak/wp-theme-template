423c046ad5818ce5b848999465f4d8965acfa951
jmucak <jmucak22@gmail.com>
2024-10-17 12:39:19 +0200

    add comment update test block png

--- app/services/blocks/MovieListBlockService.php
+++ app/services/blocks/MovieListBlockService.php
@@ -8,6 +8,7 @@ use wsytesTheme\services\BlockService;
 class MovieListBlockService extends BlockService {
 	public function get_view( array $block, string $content, bool $is_preview = false, int $post_id = 0 ): void {
 		if ( $is_preview ) {
+			/*in case that this is preview as said in https://support.advancedcustomfields.com/forums/topic/register-block-preview-image-with-acf_register_block_type/#post-146180 then dont render the block but show the preview*/
 			if ( isset( $block['data']['preview_image_help'] ) ) {
 				echo "<img src='" . $block['data']['preview_image_help'] . "' style='width:100%; height:auto;''>";
 
